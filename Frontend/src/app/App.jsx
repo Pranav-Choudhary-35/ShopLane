c653b4654b2ce036d43c091b84619d2b1660f312
Pranav-Choudhary-35 <pranavchoudhary498@gmail.com>
2026-04-27 12:33:49 +0530

    feat: add react-router dependency and configure base application routes

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,9 +1,14 @@
 import '../app/index.css'
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">app</h1>
+    <RouterProvider router={routes} >
+      <h1 className="text-3xl font-bold underline">app</h1>
+
+    </RouterProvider>
   )
 }
 
